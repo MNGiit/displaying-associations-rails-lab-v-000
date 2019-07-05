920ab6384a4fb62f11e2f60a65e258ba1ba084e6
Mohammed Noori <kmanoorib@gmail.com>
2019-07-05 19:30:22 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,8 +3,9 @@ class ArtistsController < ApplicationController
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
-
+                           
   def new
     @artist = Artist.new
   end
